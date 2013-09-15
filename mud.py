7f397c72ec7ade48fd67868d28c0b53964816bef
David Keijser <keijser@gmail.com>
2013-09-15 15:41:04 +0200

    prepare by using futures and coroutines

--- mud.py
+++ mud.py
@@ -1,56 +1,90 @@
 from tornado import ioloop, web, websocket
+from tornado.gen import coroutine, Task
 import json
+import time
 
 class User(object):
     def __init__(self, name, quest):
         self.name = name
         self.quest = quest
 
+    @coroutine
+    def look(self, what=None):
+        yield Task(lambda callback: callback(time.sleep(0.2)))
+        return "You are in a pitch black room, you are here to %s" % self.quest
+
     @classmethod
+    @coroutine
     def get(cls, name, quest):
+        yield Task(lambda callback: callback(time.sleep(0.2)))
         return cls(name.lower(), quest.lower())
 
 
 class WebSocket(websocket.WebSocketHandler):
     def open(self):
-        self.proc = self.greeting()
-        next(self.proc)
+        self.user = None
+        self.name = None
+        self.quest = None
+        self.start()
 
     def write_json(self, data):
         self.write_message(json.dumps(data))
 
-    def greeting(self):
-        name, quest = None, None
+    def start(self):
         self.write_json({
             'message': 'WHAT.. is your name?',
             'prompt': 'Enter your name'
         })
 
-        while not name:
-            name = yield
-
-        self.write_json({
-            'message': 'And what is your quest?',
-            'prompt': 'Enter your quest'
-        })
-
-        while not quest:
-            quest = yield
-
-        self.user = User.get(name, quest)
-        self.proc = self.commands()
-        yield next(self.proc)
-
-    def commands(self):
-        while True:
+    @coroutine
+    def handle_greeting(self, message):
+        if self.name is None:
+            self.name = message
             self.write_json({
-                'message': 'hurr durr'
+                'message': 'And what is your quest?',
+                'prompt': 'Enter your quest'
             })
-            message = yield
-            # do something with message
 
+        elif self.quest is None:
+            self.quest = message
+            self.user = yield User.get(self.name, self.quest)
+
+            desc = yield self.user.look()
+            self.write_json({
+                'message': desc
+            })
+        else:
+            print('extra message to handle_greeting', message)
+
+
+    @coroutine
+    def handle_command(self, message):
+        # TODO: do something sane with message
+        parts = message.split()
+
+        if parts[0] == 'look':
+            desc = yield self.user.look()
+
+            self.write_json({
+                'message': desc
+            })
+
+        else:
+            self.write_json({
+                'message': '.. what?'
+            })
+
+    @coroutine
     def on_message(self, message):
-        self.proc.send(message)
+        print('on message', message)
+        try:
+            if not self.user:
+                yield self.handle_greeting(message)
+
+            else:
+                yield self.handle_command(message)
+        except Exception as e:
+            print(e)
 
 
 application = web.Application([
