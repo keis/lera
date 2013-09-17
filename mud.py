fa663ac81eb2e4adfcb69b922b01c8cb46eb44ec
David Keijser <keijser@gmail.com>
2013-09-17 22:31:07 +0200

    some more logging when moving around

--- mud.py
+++ mud.py
@@ -1,17 +1,43 @@
 from tornado.gen import coroutine, Task
+from tornado.ioloop import IOLoop
 import time
 import logging
 import riak
-
+import smoke
 
 logger = logging.getLogger('mud')
 starting_room = 'Tp10Fhl12GliqHtbRaBf86hPeKX'
+
+
+class World(smoke.Broker):
+    enter = smoke.signal('enter')
+    leave = smoke.signal('leave')
+
+    # https://github.com/keis/smoke/issues/1
+    def publish(self, event, **kwargs):
+        publish = super(Broker, self).publish
+        IOLoop.instance().add_callback(publish, **kwargs)
+
+
+world = World()
 
 
 class User(object):
     def __init__(self, db, data):
         self.db = db
         self.data = data
+
+        # Should perhaps be per room?
+        world.enter.subscribe(smoke.weak(self._on_enter))
+        world.leave.subscribe(smoke.weak(self._on_leave))
+
+    def _on_enter(self, user=None, room=None):
+        if user != self.key and room == self.room:
+            logger.debug('%s notices %s enters %s', self.key, user, room)
+
+    def _on_leave(self, user=None, room=None):
+        if user != self.key and room == self.room:
+            logger.debug('%s notices %s leaves %s', self.key, user, room)
 
     @property
     def key(self):
@@ -57,14 +83,20 @@ class User(object):
             return "You can't go %s" % label
         key, room = result
 
+        logger.info('%s moving from %s to %s', self.key, self.room, key[1])
         # Remove from old room
         try:
             old_occupants = yield self.db.get('occupants', self.room)
         except KeyError as e:
             pass
         else:
-            old_occupants['occupants'].remove(self.key)
-            yield self.db.save('occupants', self.room, old_occupants)
+            try:
+                old_occupants['occupants'].remove(self.key)
+            except ValueError:
+                pass
+            else:
+                yield self.db.save('occupants', self.room, old_occupants)
+                world.leave(user=self.key, room=self.room)
 
         # Update room link
         self.room = key[1]
@@ -77,6 +109,8 @@ class User(object):
             new_occupants = {'occupants': []}
         new_occupants['occupants'].append(self.key)
         yield self.db.save('occupants', self.room, new_occupants)
+        world.enter(user=self.key, room=self.room)
+        logger.info('%s moved to %s', self.key, self.room)
 
         return self.describe(room)
 
