ded612dfaf18721461dbc4ec760ac40752c03ae4
David Keijser <keijser@gmail.com>
2013-10-06 17:29:53 +0200

    move application code from html file

---
+++ js/app/socket.js
@@ -0,0 +1,29 @@
+define(['backbone', 'underscore'], function (Backbone, _) {
+    function Socket (url) {
+        var self = {},
+            sock = new WebSocket(url);
+
+        _.extend(self, Backbone.Events);
+
+        sock.onmessage = function (event) {
+            self.trigger('recv', JSON.parse(event.data));
+        };
+
+        sock.onopen = function (event) {
+            self.trigger('open');
+        }
+
+        sock.onclose = function (event) {
+            self.trigger('close');
+        };
+
+        self.send = function (data) {
+            self.trigger('send', data);
+            sock.send(data);
+        }
+
+        return self;
+    };
+
+    return Socket;
+});
---
+++ js/app/status.js
@@ -0,0 +1,10 @@
+define(['jquery'], function ($) {
+    'use strict';
+    var $status = $('#status');
+
+    return function setStatus (status, message) {
+        console.log('status changed', status);
+        $status.attr('data-status', status);
+        $status.text(message || status);
+    };
+});
