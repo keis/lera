6adf595fcc1f1b2144cd2ff8dd58e754191294ee
David Keijser <keijser@gmail.com>
2013-10-06 17:59:04 +0200

    decouple resume session logic from main app

--- js/app/main.js
+++ js/app/main.js
@@ -1,20 +1,14 @@
 define([
     './status',
     './socket',
-    './socket',
-    'backbone',
-    'underscore',
-    'jquery'
-], function (setStatus, Socket, Backbone, _, $) {
+    './session',
+], function (setStatus, Socket, session) {
     'use strict';
 
     var sock = new Socket('ws://localhost:8888/socket'),
         form = document.querySelector('form[name=command]'),
         commandInput = form.querySelector('input[name=command]'),
         messages = document.querySelector('#messages'),
-        user = {},
-        loggedIn = false,
-        prompt,
         setStatus;
 
     function showMessage (message, command) {
@@ -27,37 +21,15 @@ define([
     }
 
     function sendMessage (message) {
-        showMessage(message, true);
         sock.send(message);
-
-        if (!loggedIn && prompt) {
-            console.log('message during login', message);
-            if (prompt.match(/name$/)) {
-                user.name = message;
-            } else if (prompt.match(/quest$/)) {
-                user.quest = message;
-            }
-        }
     }
 
-    function loadUserData () {
-        var data = window.localStorage['user'];
-        if (data) {
-            try {
-                user = JSON.parse(data);
-            } catch (err) {
-                console.log(err);
-            }
-        }
-        console.log('user information', user);
+    try {
+        session.resume(sock);
+    } catch (err) {
+        console.log("Could not resume session");
+        session.watch(sock);
     }
-
-    function saveUserData () {
-        console.log('saving user information', user);
-        window.localStorage['user'] = JSON.stringify(user);
-    }
-
-    loadUserData();
     setStatus('connecting');
     commandInput.disabled = true;
 
@@ -68,28 +40,20 @@ define([
         showMessage(data.message);
         commandInput.disabled = (prompt === '<disabled>')
         commandInput.placeholder = prompt;
+    });
 
-        if (!loggedIn && user.name && user.quest) {
-            if (prompt.match(/name$/)) {
-                sendMessage(user.name);
-            } else if (prompt.match(/quest$/)) {
-                sendMessage(user.quest);
-            } else if (data.message.match(/welcome/i)) {
-                loggedIn = true;
-                saveUserData();
-            }
-        }
+    sock.on('send', function (message) {
+        showMessage(message, true);
     });
 
     sock.on('close', function () {
         setStatus('error', 'disconnected');
-        loggedIn = false,
         commandInput.disabled = true;
     });
 
     sock.on('open', function () {
         setStatus('connected');
-        loggedIn = false,
+
         commandInput.disabled = false;
         commandInput.placeholder = '';
     });
---
+++ js/app/session.js
@@ -0,0 +1,79 @@
+define(function () {
+    function loadUserData () {
+        var data = window.localStorage['user'],
+            user;
+
+        if (data) {
+            try {
+                user = JSON.parse(data);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        console.log('user information', user);
+        return user;
+    }
+
+    function saveUserData (user) {
+        console.log('saving user information', user);
+        window.localStorage['user'] = JSON.stringify(user);
+    }
+
+    function isValidUser (user) {
+        return user && user.name && user.quest;
+    }
+
+    return {
+        watch: function (sock) {
+            var prompt = '',
+                user = {};
+
+            function onrecv (data) {
+                prompt = data.prompt || '';
+            }
+
+            function onsend (message) {
+                console.log('message during login', message);
+
+                if (prompt.match(/name$/)) {
+                    user.name = message;
+                } else if (prompt.match(/quest$/)) {
+                    user.quest = message;
+                } else {
+                    sock.off('send', onsend);
+                    sock.off('recv', onrecv);
+                }
+
+                if (isValidUser(user)) {
+                    saveUserData(user);
+                }
+            }
+
+            sock.on('send', onsend);
+            sock.on('recv', onrecv);
+        },
+
+        resume: function (sock) {
+            var user = loadUserData();
+            if (!isValidUser(user)) {
+                throw new Error('no existing user data');
+            }
+
+            function onrecv(data) {
+                console.log("ON RECV");
+                var prompt = data.prompt || '';
+
+                if (prompt.match(/name$/)) {
+                    sock.send(user.name);
+                } else if (prompt.match(/quest$/)) {
+                    sock.send(user.quest);
+                } else if (data.message.match(/welcome/i)) {
+                    sock.off('recv', onrecv);
+                    saveUserData();
+                }
+            }
+
+            sock.on('recv', onrecv);
+        }
+    };
+});
