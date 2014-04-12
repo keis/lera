d2c4fc3890364e266f5ee0add388fc4fcfb61e40
David Keijser <keijser@gmail.com>
2014-04-12 15:17:25 +0200

    add small tool for testing

---
+++ test.js
@@ -0,0 +1,23 @@
+var Client = require('websocket').client,
+    client = new Client();
+
+client.on('connect', function (sock) {
+    messages = [
+        'foo',
+        'foo'
+    ];
+
+    console.error("connected!");
+    sock.on('message', function (data) {
+        console.log('got data', data);
+        var msg;
+        while ((msg = messages.shift())) {
+            sock.send(msg);
+        }
+    });
+    sock.on('close', function () {
+        console.error('disconnected');
+    });
+});
+
+client.connect('ws://localhost:8888/socket');
