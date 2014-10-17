1d58e36b32a7cccf0cd60c4d739723935ae604df
David Keijser <keijser@gmail.com>
2014-10-17 17:26:22 +0200

    pulsar allow some shortcuts in routes

--- pulsar_werkzeug.py
+++ pulsar_werkzeug.py
@@ -5,22 +5,45 @@ from werkzeug.wrappers import Request
 from trollius import coroutine, Future, Task, sleep
 
 
+from werkzeug.http import HTTP_STATUS_CODES
+from werkzeug.datastructures import Headers
+
 class Response(Future):
     '''connect/express inspired response object'''
 
-    def __init__(self, start_response):
-        super(Response, self).__init__()
-        self.start_response = start_response
-        self.headers = {}
+    default_status = 200
+    default_mimetype = 'text/plain'
 
-    def status(self, status):
-        headers = list(self.headers.items())
-        self.start_response(status, headers)
+    @property
+    def status_code(self):
+        return self._status_code
+
+    @status_code.setter
+    def status_code(self, code):
+        self._status_code = code
+        try:
+            self._status = '%d %s' % (code, HTTP_STATUS_CODES[code].upper())
+        except KeyError:
+            self._status = '%d UNKNOWN' % code
+
+    def __init__(self, environ, start_response):
+        super(Response, self).__init__()
+        self.environ = environ
+        self.start_response = start_response
+        self.headers = Headers()
+        self.status_code = self.default_status
+        self.headers['Content-Type'] = self.default_status
+
+    def status(self, code):
+        self.status_code = code
         return self
 
     def send(self, result):
         if isinstance(result, bytes):
             result = [result]
+
+        headers = self.headers.to_wsgi_list()
+        self.start_response(self._status, headers)
 
         self.set_result(result)
         return self
@@ -28,15 +51,13 @@ class Response(Future):
 
 @coroutine
 def index(req, res):
-    res.headers['Content-Type'] = 'text/plain'
-    res.status('200 OK').send(b'index')
+    res.send(b'index')
 
 
 @coroutine
 def lazy(req, res):
-    res.headers['Content-Type'] = 'text/plain'
     yield from sleep(1)
-    res.status('200 Ok').send([b'lazy page'])
+    res.status(201).send([b'lazy page'])
 
 
 urls = Map([
@@ -56,7 +77,7 @@ def dispatch_request(req, res):
 
 def app(environ, start_response):
     request = Request(environ)
-    response = Response(start_response)
+    response = Response(environ, start_response)
     Task(dispatch_request(request, response))
     return response
 
