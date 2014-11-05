9a12b6d71b84b90f22181cbe6e6af8f57043d13b
David Keijser <keijser@gmail.com>
2014-11-05 17:39:21 +0100

    add test of messy qube case

--- tests/test_qube.py
+++ tests/test_qube.py
@@ -1,7 +1,7 @@
 from hamcrest import assert_that, equal_to
 from hamcrest.library import has_entry, has_length
 from contextlib import contextmanager
-
+from pprint import pprint
 import qube
 
 def track_error():
@@ -65,3 +65,79 @@ def test_threeway_merge():
     assert_that(m, has_entry('data', has_entry('value', 9)))
 
     assert_that(error, has_length(0))
+
+
+def test_step_merge():
+    qa = qube.init({'name': 'zoidberg', 'number': 5, 'value': 10})
+    qube.apply_op(qa, ('change', 'number', (5, 6)))
+
+    qb = qube.from_json(qube.to_json(qa))
+
+    qube.apply_op(qa, ('change', 'name', ('zoidberg', 'bob')))
+    qube.apply_op(qb, ('change', 'number', (6, 7)))
+
+    qc = qube.from_json(qube.to_json(qa))
+    qube.apply_op(qc, ('change', 'value', (10, 9)))
+
+    pprint(qa)
+    pprint(qb)
+    pprint(qc)
+
+    error, watch = track_error()
+    ma = qube.merge(qa, qb, error=watch)
+
+    assert_that(error, has_length(0))
+    assert_that(ma, has_entry('sequence', 3))
+    assert_that(ma, has_entry('data', has_entry('name', 'bob')))
+    assert_that(ma, has_entry('data', has_entry('number', 7)))
+    assert_that(ma, has_entry('data', has_entry('value', 10)))
+
+    print('---')
+    pprint(ma)
+    pprint(qc)
+
+    error, watch = track_error()
+    mb = qube.merge(ma, qc, error=watch)
+    assert_that(error, has_length(0))
+    assert_that(mb, has_entry('sequence', 4))
+    assert_that(mb, has_entry('data', has_entry('name', 'bob')))
+    assert_that(mb, has_entry('data', has_entry('number', 7)))
+    assert_that(mb, has_entry('data', has_entry('value', 9)))
+
+
+def test_step_merge_bside():
+    qa = qube.init({'name': 'zoidberg', 'number': 5, 'value': 10})
+    qube.apply_op(qa, ('change', 'number', (5, 6)))
+
+    qb = qube.from_json(qube.to_json(qa))
+
+    qube.apply_op(qa, ('change', 'name', ('zoidberg', 'bob')))
+    qube.apply_op(qb, ('change', 'number', (6, 7)))
+
+    qc = qube.from_json(qube.to_json(qb))
+    qube.apply_op(qc, ('change', 'value', (10, 9)))
+
+    pprint(qa)
+    pprint(qb)
+    pprint(qc)
+
+    error, watch = track_error()
+    ma = qube.merge(qa, qb, error=watch)
+
+    assert_that(error, has_length(0))
+    assert_that(ma, has_entry('sequence', 3))
+    assert_that(ma, has_entry('data', has_entry('name', 'bob')))
+    assert_that(ma, has_entry('data', has_entry('number', 7)))
+    assert_that(ma, has_entry('data', has_entry('value', 10)))
+
+    print('---')
+    pprint(ma)
+    pprint(qc)
+
+    error, watch = track_error()
+    mb = qube.merge(ma, qc, error=watch)
+    assert_that(error, has_length(0))
+    assert_that(mb, has_entry('sequence', 4))
+    assert_that(mb, has_entry('data', has_entry('name', 'bob')))
+    assert_that(mb, has_entry('data', has_entry('number', 7)))
+    assert_that(mb, has_entry('data', has_entry('value', 9)))
